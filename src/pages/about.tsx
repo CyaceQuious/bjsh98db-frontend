60e207f23df921b834b83c77de158e5e8c9977e1
czyarl <chenzhey22@mails.tsinghua.edu.cn>
2025-04-09 13:22:38 +0800

    为About页添加更多信息

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,6 +1,7 @@
 import { Typography, Alert, List, Layout, Spin, theme } from 'antd';
 import { useRouter } from 'next/router';
 import styles from '../styles/container.module.css';
+import AboutCard from '../components/AboutCard';
 import Link from 'next/link';
 import Card from 'antd/es/card/Card';
 
@@ -18,77 +19,9 @@ const About = () => {
         </div>
     );
 
-    const Developers = [
-        { name: 'ryz', link: '/' , other: ''},
-        { name: 'czy', link: '/' , other: ''},
-        { name: 'zjy', link: '/' , other: ''},
-        { name: 'fgf', link: '/' , other: ''},
-        { name: 'mpc', link: '/' , other: ''}
-    ]
-
     return (
-        <Content className={styles.container} style={{ padding: token.paddingLG }}>
-            <div style={{ 
-                maxWidth: 800, 
-                margin: '0 auto',
-                padding: token.paddingLG,
-                background: token.colorBgContainer,
-                borderRadius: token.borderRadiusLG,
-                boxShadow: token.boxShadow
-            }}>
-                <Title
-                    level={2}
-                    style={{
-                        textAlign: 'center',
-                        marginBottom: token.marginLG,
-                    }}
-                >
-                    关于数据平台
-                </Title>
-                
-                <Card title="项目介绍" style={{marginBottom: token.marginLG}}>
-                bjsh98.db（大体协数据库）是一个为北京市大学生体育协会（简称“大体协”）成绩查询页开发的数据检索平台，借助于信息化技术，尤其是软件技术和互联网技术，支持赛会组织方、运动员、观众等群体高效、安全地上传、修正、查询相关赛事成绩数据，优化落后的查询模式。
-                </Card>
-
-                <Card title="开发者"  style={{marginBottom: token.marginLG}} >
-                    <List
-                        grid={{ gutter: 16, column: 2 }}
-                        dataSource={Developers}
-                        renderItem={(item) => (
-                            <List.Item style={{ margin: 0 }}>
-                                <Link 
-                                    href={item.link} 
-                                    style={{
-                                        color: token.colorPrimary,
-                                        transition: 'color 0.3s',
-                                    }}
-                                >
-                                    {item.name}
-                                </Link>
-                                <Text>
-                                    {item.other}
-                                </Text>
-                            </List.Item>
-                        )}
-                    />
-                </Card>
-
-                <Alert
-                    message="数据声明"
-                    description={
-                        <Text style={{ color: token.colorTextSecondary }}>
-                            原始数据来源：
-                            <Link href="https://wx.bjsh98.com/client/index.html">
-                                bjsh98官方平台
-                            </Link>
-                        </Text>
-                    }
-                    type="info"
-                    showIcon
-                />
-            </div>
-        </Content>
-    );
+        <AboutCard mode='full'/>
+    )
 };
 
 export default About;
