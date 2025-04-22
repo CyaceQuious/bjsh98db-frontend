6b2ee4840121bae870a3bdcb8bc85a9b77afa141
czyarl <chenzhey22@mails.tsinghua.edu.cn>
2025-04-22 10:25:18 +0800

    修改比赛成绩的时候不让他动组织名称

--- src/components/SearchResultTable.tsx
+++ src/components/SearchResultTable.tsx
@@ -111,7 +111,7 @@ export default function SearchResultTable({
         width: 100,
         render: (_, record) => (
             (isSystemAdmin || allContestOfficial.includes(record.mid)) && <div style={{ display: 'flex', alignItems: 'center', gap: 1 }}>
-                <ResultEditForm defaultValues={record} isEditMode onSuccess={onContentReFresh} frozenItems={["meet", "projectname", "name"]}/>
+                <ResultEditForm defaultValues={record} isEditMode onSuccess={onContentReFresh} frozenItems={["meet", "projectname", "name", "groupname"]}/>
                 <Button
                     type="link"
                     onClick={() => handleDeleteClick({...record} as DeleteRequest)}
