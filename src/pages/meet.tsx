5c08aec194a4654003b32feb713a12b5e2d17994
czyarl <chenzhey22@mails.tsinghua.edu.cn>
2025-05-24 21:25:42 +0800

    为比赛主页添加目录方便浏览

--- src/pages/meet.tsx
+++ src/pages/meet.tsx
@@ -1,7 +1,7 @@
 import Head from 'next/head';
 import { useRouter } from 'next/router';
 import { useState } from 'react';
-import { List, Tag, Timeline } from 'antd';
+import { Timeline } from 'antd';
 import GroupScoreTable from '../components/GroupScoreTable';
 import MeetProjectTable from '../components/MeetProjectTable';
 import MeetManage from '../components/MeetManage';
